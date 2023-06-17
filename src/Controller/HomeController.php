ec7a06cca2bd69fc43856ef94819561c9e11a351
OHLAntoine <antoine.ohl@gmail.com>
2023-06-17 17:20:13 +0200

    Affichage des horaires sur l'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Planning;
 use App\Entity\Service;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,11 +14,17 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(ManagerRegistry $doctrine): Response
     {
-        $repository = $doctrine->getRepository(Service::class);
-        $services = $repository->findAll();
+        //Récupération des services
+        $repositoryService = $doctrine->getRepository(Service::class);
+        $services = $repositoryService->findAll();
+
+        //Récupération du planning d'ouverture
+        $repositoryPlanning = $doctrine->getRepository(Planning::class);
+        $horaires = $repositoryPlanning->findAll();
 
         return $this->render('home/index.html.twig', [
             "services" => $services,
+            "horaires" => $horaires,
         ]);
     }
 }
