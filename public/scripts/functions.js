0e32a997af2a7115134d1a1293bd108f32962d7e
OHLAntoine <antoine.ohl@gmail.com>
2023-06-24 12:15:42 +0200

    Ajout double range slider pour filtres

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -26,3 +26,47 @@ const toggleConfirmPassword = () => {
 
 // Double range sliders
 
+// Prix
+$( function() {
+    $( "#price-slider-range" ).slider({
+        range: true,
+        min: 0,
+        max: 15000,
+        values: [ 0, 15000 ],
+        slide: function( event, ui ) {
+            $( "#price-amount" ).val( ui.values[ 0 ] + "€ - " + ui.values[ 1 ] + "€");
+        }
+        });
+        $( "#price-amount" ).val($( "#price-slider-range" ).slider( "values", 0 ) +
+        "€ - " + $( "#price-slider-range" ).slider( "values", 1 ) + "€");
+    } );
+
+// Kilométrage
+$( function() {
+    $( "#km-slider-range" ).slider({
+        range: true,
+        min: 0,
+        max: 15000,
+        values: [ 0, 15000 ],
+        slide: function( event, ui ) {
+            $( "#km-amount" ).val( ui.values[ 0 ] + "km - " + ui.values[ 1 ] + "km");
+        }
+        });
+        $( "#km-amount" ).val($( "#km-slider-range" ).slider( "values", 0 ) +
+        "km - " + $( "#km-slider-range" ).slider( "values", 1 ) + "km");
+    } );
+
+// Année
+$( function() {
+    $( "#year-slider-range" ).slider({
+        range: true,
+        min: 1990,
+        max: 2023,
+        values: [ 0, 15000 ],
+        slide: function( event, ui ) {
+            $( "#year-amount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+        }
+        });
+        $( "#year-amount" ).val($( "#year-slider-range" ).slider( "values", 0 ) +
+        " - " + $( "#year-slider-range" ).slider( "values", 1 ));
+    } );
