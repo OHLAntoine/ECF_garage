b3c2e4b810fa5f5ab67afd6a696ab89ab813592d
OHLAntoine <antoine.ohl@gmail.com>
2023-06-24 10:55:26 +0200

    Ajustement contact via titre annonce

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Entity\Planning;
 use App\Form\ContactType;
+use App\Repository\CarRepository;
+use App\Repository\ServiceRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -47,6 +49,84 @@ class ContactController extends AbstractController
         return $this->render('contact/form.html.twig', [
             "horaires" => $horaires,
             "contact_form" => $form->createView(),
+            "car" => null,
+            "service" => null,
+        ]);
+    }
+
+    #[Route('/contact-us-car/{id<\d+>}', name: 'contact-us-car')]
+    public function carContact(ManagerRegistry $doctrine, Request $request, MailerInterface $mailer, CarRepository $repo, int $id): Response
+    {
+        //Récupération du planning d'ouverture
+        $repositoryPlanning = $doctrine->getRepository(Planning::class);
+        $horaires = $repositoryPlanning->findAll();
+
+        $car = $repo->findOneBy(["id" => $id]);
+
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new TemplatedEmail())
+            ->from($contact->getEmail())
+            ->to('garage-parrot@gmail.com')
+            ->subject($contact->getSubject() ? $contact->getSubject() : "Demande d'informations")
+            ->htmlTemplate('emails/contact.html.twig')
+            ->context([
+                'firstname' => $contact->getFirstname(),
+                'lastname' => $contact->getLastname(),
+                'telephone' => $contact->getTelNumber(),
+                'message' => $contact->getMessage(),
+                ]);
+        
+            $mailer->send($email);
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('contact/form.html.twig', [
+            "horaires" => $horaires,
+            "contact_form" => $form->createView(),
+            "car" => $car,
+            "service" => null,
+        ]);
+    }
+
+    #[Route('/contact-us-service/{id<\d+>}', name: 'contact-us-service')]
+    public function serviceContact(ManagerRegistry $doctrine, Request $request, MailerInterface $mailer, ServiceRepository $repo, int $id): Response
+    {
+        //Récupération du planning d'ouverture
+        $repositoryPlanning = $doctrine->getRepository(Planning::class);
+        $horaires = $repositoryPlanning->findAll();
+
+        $service = $repo->findOneBy(["id" => $id]);
+
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new TemplatedEmail())
+            ->from($contact->getEmail())
+            ->to('garage-parrot@gmail.com')
+            ->subject($contact->getSubject() ? $contact->getSubject() : "Demande d'informations")
+            ->htmlTemplate('emails/contact.html.twig')
+            ->context([
+                'firstname' => $contact->getFirstname(),
+                'lastname' => $contact->getLastname(),
+                'telephone' => $contact->getTelNumber(),
+                'message' => $contact->getMessage(),
+                ]);
+        
+            $mailer->send($email);
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('contact/form.html.twig', [
+            "horaires" => $horaires,
+            "contact_form" => $form->createView(),
+            "service" => $service,
+            "car" => null,
         ]);
     }
 }
